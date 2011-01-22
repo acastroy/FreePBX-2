2084856ccb7e551b78049ad0da894c4858d68dfa
mickecarlsson <mickecarlsson@05cd7311-dd0d-0410-8c4e-c7a75d82e046>
2011-01-22 14:49:16 +0000

    Revert r11060 wrong branch. However, that helped me to figure out why it was failing
    git-svn-id: http://svn.freepbx.org/freepbx/branches/2.8@11062 05cd7311-dd0d-0410-8c4e-c7a75d82e046

--- amp_conf/htdocs/admin/config.php
+++ amp_conf/htdocs/admin/config.php
@@ -295,8 +295,6 @@ if (($display == 'index') && ($cur_menuitem['module']['rawname'] == 'builtin')) 
 }
 
 
-// set the language so local module languages take
-set_language();
 
 // show the appropriate page
 switch($display) {
@@ -389,6 +387,9 @@ if ($quietmode) {
 	$admin_template['fpbx_usecategories'] = $amp_conf['USECATEGORIES'];
 	$admin_template['fpbx_type'] = $type;
 	$admin_template['display'] = $display;
+
+	// set the language so local module languages take
+	set_language();
 
 	// then load it and put it into the main freepbx interface
 	$template['content'] = loadview('freepbx_admin', $admin_template);

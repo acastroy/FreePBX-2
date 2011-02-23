2ac8d4f1d531ce564115e3e143df87f0397b1e3d
p_lindheimer <p_lindheimer@05cd7311-dd0d-0410-8c4e-c7a75d82e046>
2011-02-23 20:40:25 +0000

    fixes #4861 don't use FreePBX authentication when accessing ARI, it provides its own
    git-svn-id: http://svn.freepbx.org/freepbx/branches/2.9@11574 05cd7311-dd0d-0410-8c4e-c7a75d82e046

--- amp_conf/htdocs/recordings/index.php
+++ amp_conf/htdocs/recordings/index.php
@@ -4,6 +4,7 @@
  * @file
  * main
  */
+$bootstrap_settings['freepbx_auth'] = false;
 if (!@include_once(getenv('FREEPBX_CONF') ? getenv('FREEPBX_CONF') : '/etc/freepbx.conf')) {
 	include_once('/etc/asterisk/freepbx.conf');
 }

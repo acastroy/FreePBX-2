df61e7d7f9083697b54719edcfc82e92d42440cb
p_lindheimer <p_lindheimer@05cd7311-dd0d-0410-8c4e-c7a75d82e046>
2011-02-24 04:51:19 +0000

    fixes #4841 move bootstrap code from index.php to ARI's bootstrap.php
    git-svn-id: http://svn.freepbx.org/freepbx/branches/2.9@11581 05cd7311-dd0d-0410-8c4e-c7a75d82e046

--- amp_conf/htdocs/recordings/index.php
+++ amp_conf/htdocs/recordings/index.php
@@ -4,10 +4,6 @@
  * @file
  * main
  */
-$bootstrap_settings['freepbx_auth'] = false;
-if (!@include_once(getenv('FREEPBX_CONF') ? getenv('FREEPBX_CONF') : '/etc/freepbx.conf')) {
-	include_once('/etc/asterisk/freepbx.conf');
-}
 include_once("includes/bootstrap.php");
 ariPageHeader();
 include_once("includes/common.php");
